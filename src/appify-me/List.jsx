b84bd6316e59142eff23bcf645d9ece4b32681b6
Kye Buffery <kyebuff@Kyes-MacBook-Pro.local>
2017-11-27 14:49:12 +0000

    re-usable list component

--- src/appify-me/List.jsx
+++ src/appify-me/List.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-let listGroup = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
-
-const ListGroup = () => (
+const ListGroup = (props) => (
 		<ul className="list-group">
-			{listGroup.map((el, index) => {
-				return (<li key={index} className="list-group-item">{el}</li>)		
+			{props.array.map((el, index) => {
+				return (<li key={index} className={props.classTitle}>{el}</li>)		
 			})}
 		</ul>
 );
--- src/appify-me/Main.jsx
+++ src/appify-me/Main.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ListGroup from './ListGroup';
 import Pagination from './Pagination';
+import List from './List';
+
+const pages = [1, 2, 3, 4];
+const listGroup = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
 
 const Main = () => (
 	<main className="col-md-8">
 		<p className="lead text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Huius, Lyco, oratione locuples, rebus ipsis ielunior.</p>
 		<p className="text-left">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-		<ListGroup />
-		<Pagination />
+		<List classTitle="list-group-item" array={listGroup} />
+		<List classTitle="pagination" array={pages} />
+
 	</main>
 );
 
