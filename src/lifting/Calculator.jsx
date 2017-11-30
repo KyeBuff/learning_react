3506647e845bcca6121170eb2b7508e4656388f9
KyeBuff <kye.buffery@btinternet.com>
2017-11-30 09:54:06 +0000

    Fixed full tracking and now at pre-memory functions

--- src/lifting/Calculator.jsx
+++ src/lifting/Calculator.jsx
@@ -7,15 +7,17 @@ class Calculator extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			output: [],
+			outputArr: [],
 			currVal: '',
+			currOp: null,
 			total: 0,
-			// Index allows us to target previous object to track initial/next value of operation
+			// Index allows us to target previous object to track value of operation
 			index: -1,
 		}
 		this.numOnClick = this.numOnClick.bind(this);
 		this.onOpClick = this.onOpClick.bind(this);
 		this.equals = this.equals.bind(this);
+
 	}
 
 	numOnClick(n) {
@@ -26,15 +28,15 @@ class Calculator extends Component {
 
 	onOpClick(op) {
 
-		const arr = this.state.output.slice();
+		const arr = this.state.outputArr.slice();
 
 		let numOb = {
-			//if output is empty, just assign the curret number to initial
-			initial: !arr.length ?
+			//if outputArr is empty, just assign the curret number to value
+			value: !arr.length ?
 								 +this.state.currVal 
 						  	: 
-						  	//if there is an initial value in the prev ob
-						  	//pass previous initial value
+						  	//if there is an value value in the prev ob
+						  	//pass previous value value
 						  	//pass currVal  
 							  this.calculate(arr[this.state.index], +this.state.currVal),
 			op: op,
@@ -47,7 +49,15 @@ class Calculator extends Component {
 
 		//set opClicked to true so that we can't use multiple ops
 		//Increase index state so we are always tracking the previous ob in arr
-		this.setState({output: arr, currVal: '', opClicked: true, index: this.state.index + 1});
+		this.setState(
+			{
+				outputArr: arr, 
+				currVal: '', 
+				currOp: op, 
+				opClicked: true, 
+				index: this.state.index + 1
+			}
+		);
 
 	}
 
@@ -55,8 +65,8 @@ class Calculator extends Component {
 
 		//take ob and currVal
 
-		//initial value passed to total
-		let total = numOb.initial;
+		//value value passed to total
+		let total = numOb.value;
 
 		//use numOb operator on currVal in switch
 		switch(numOb.op) {
@@ -80,14 +90,24 @@ class Calculator extends Component {
 
 	equals() {
 
+		const finalOb = {
+			value: +this.state.currVal,
+		}
+
 		//calculate again using tracked value in latest ob and currVal
-		this.setState({total: this.calculate(this.state.output[this.state.index], +this.state.currVal)});
+		this.setState(
+			{
+				total: this.calculate(this.state.outputArr[this.state.index], +this.state.currVal
+					)
+			}
+		);
 
 	}
 
   render() {
   	const numButtons = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   	const operators = ['+', '-', 'X', '/'];
+  	const memoryBtns = ['M+', 'MC', 'MRC']
   	return (
   		<div>
 	  		<div>
@@ -105,8 +125,9 @@ class Calculator extends Component {
 	  			}
 	  			<button className='btn btn-success' onClick={this.equals}>=</button> 
 	  		</div>
-	  		<p>{this.state.currVal || 0}</p>
-	  		<p>{this.state.total}</p>
+	  		<p>Value: {this.state.currVal}</p>
+	  		<p>Operator: {this.state.currOp}</p>
+	  		<p>Total: {this.state.total}</p>
   		</div>
   	);
   }
